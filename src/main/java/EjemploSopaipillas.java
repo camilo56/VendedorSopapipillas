fecac31655fe970345d08be8839556dd4d5a38f2
camilo56 <76082597+camilo56@users.noreply.github.com>
2021-05-08 19:32:13 -0400

    3 commit

--- src/main/java/EjemploSopaipillas.java
+++ src/main/java/EjemploSopaipillas.java
@@ -1,9 +1,31 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class EjemploSopaipillas {
-    public static void Main(String[] args ){
+    public static void main(String[] args ){
         Scanner teclado =new Scanner(System.in);
-        VendedorSopapipillas miVendedor =new VendedorSopapipillas(36,150);
+        Random rnum =new Random();
+        VendedorSopapipillas miVendedor =new VendedorSopapipillas(36,150,5166);
+
+         System.out.println("Cuantas sopaipillas de_*"+miVendedor.getPrecio()+"*_pesos desea comprar:");
+
+
+         int cant = teclado.nextInt();
+         int rest= miVendedor.getInventarioIncial()- cant;
+         miVendedor.setInventarioIncial(rest);
+
+
+
+
+       int a= miVendedor.pagarCliente(cant);
+
+       System.out.println(" ingrese con cuanto pagara:\n");
+       int pagoCliente = teclado.nextInt();
+
+       miVendedor.vuelto(pagoCliente,a);
+
+       miVendedor.setInventarioIncial();
+
 
 
 
--- src/main/java/VendedorSopapipillas.java
+++ src/main/java/VendedorSopapipillas.java
@@ -1,19 +1,36 @@
+import java.util.Scanner;
+
 public class VendedorSopapipillas
 
 {
+    Scanner teclado =new Scanner(System.in);
+
     private int inventarioIncial;
     private int precio;
+    private int dineroInicial;
 
     public VendedorSopapipillas() {
         this.inventarioIncial = 50;
         this.precio = 200;
+        this.dineroInicial=5000;
+
 
     }
-    public VendedorSopapipillas(int inventarioIncial, int precio) {
+
+
+    public VendedorSopapipillas(int inventarioIncial, int precio, int dineroInicial) {
         this.inventarioIncial = inventarioIncial;
         this.precio = precio;
+        this.dineroInicial = dineroInicial;
     }
 
+    public int getDineroInicial() {
+        return dineroInicial;
+    }
+
+    public void setDineroInicial(int dineroInicial) {
+        this.dineroInicial = dineroInicial;
+    }
 
     public int getInventarioIncial() {
         return inventarioIncial;
@@ -30,6 +47,30 @@ public class VendedorSopapipillas
     public void setPrecio(int precio) {
         this.precio = precio;
     }
+
+
+    public int vuelto(int pago,int aPagar){
+
+        int b=pago-aPagar;
+        System.out.println("El vuelto del cliente es:"+b);
+        return b;
+    }
+
+
+    public int cantSopa(){
+        System.out.println("Cuantas sopaipillas de_*"+precio+"*_pesos desea comprar:");
+
+        int cant = teclado.nextInt();
+        return cant;
+    }
+public int pagarCliente(int cant) {
+    int pagarCliente = cant * precio;
+    System.out.println("monto que debe pagar:\n" + pagarCliente);
+    return pagarCliente;
+
+}
+
+
 
 
 
